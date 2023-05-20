7d6a515968262b6f3a1a010e94bbafb36c43620a
pbaki <pbaki999@gmail.com>
2023-05-20 16:06:29 +0200

    now sorting before making tree

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+//Merge sort
+function MergeSort(arr) {
+  let mid = arr.length / 2;
+  let first = arr.slice(0, mid);
+  let second = arr.slice(mid, arr.length);
+  if (arr.length < 2) {
+    return arr;
+  }
+  let left = MergeSort(first);
+  let right = MergeSort(second);
+  let finish = merge(left, right);
+  return finish;
+}
+
+function merge(arr1, arr2) {
+  let mergeArr = [];
+  while (arr1.length >= 1 && arr2.length >= 1) {
+    if (arr1[0] < arr2[0]) {
+      mergeArr.push(arr1.shift());
+    } else if (arr1[0] == arr2[0]) {
+      mergeArr.push(arr1[0]);
+      arr1.shift();
+      arr2.shift();
+    } else {
+      mergeArr.push(arr2.shift());
+    }
+  }
+  if (arr1.length == 0 && arr2.length !== 0) {
+    mergeArr = mergeArr.concat(arr2);
+  } else if (arr2.length == 0 && arr1.length !== 0) {
+    mergeArr = mergeArr.concat(arr1);
+  }
+  return mergeArr;
+}
+
 class Node {
   constructor(data) {
     this.data = data;
@@ -53,7 +88,11 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-let bst = new Tree(arr);
+let arr = [
+  1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 6, 2, 7, 235, 7845, 1234,
+  346, 856,
+];
+let sortedArr = MergeSort(arr);
+let bst = new Tree(sortedArr);
 let sortedBST = sortDupli(bst);
 prettyPrint(sortedBST.root);
